ad9bfe448edd739d93c370629160e3edc2535f8d
Khai Zulfa <khaizulfa18@gmail.com>
2024-07-31 10:18:47 +0700

    add dashboard documentation swal & toast

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,10 +1,35 @@
 import Card from '@/Components/Card';
+import useSweetAlert from '@/Hooks/useSwal';
+import useToast from '@/Hooks/useToast';
 import AppLayout from '@/Layouts/AppLayout';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Button } from '@headlessui/react';
 import { Head } from '@inertiajs/react';
 
 export default function Dashboard({ auth }) {
+
+    const { showToast } = useToast();
+
+    const { showAlert } = useSweetAlert();
+
+    const handleAlert = async (option) => {
+
+        const result = await showAlert(option);
+
+        if(result && result.isConfirmed){
+            // proceed to delete
+            console.log('proceed to delete');
+
+            await showAlert({
+                title: 'Deleted!',
+                text: 'Your file has been deleted.',
+                icon: 'success',
+                showConfirmButton: false,
+                timer: 1500,
+            });
+        }
+    }
+
     return (
         <AppLayout>
             <Head title="Dashboard" />
@@ -34,6 +59,25 @@ export default function Dashboard({ auth }) {
                         <span className="badge badge-warning badge-outline">Warning</span>
                         <span className="badge badge-error badge-outline">Error</span>
                         <span className="badge badge-link badge-outline">Link</span>
+                    </div>
+                    <div className="flex gap-2 mt-2">
+                        <Button className={'btn btn-sm btn-success'} onClick={() => showToast('Success, something went right!')}>Success Toast</Button>
+                        <Button className={'btn btn-sm btn-error'} onClick={() => showToast('Error, something went wrong!','error')}>Error Toast</Button>
+                        <Button className={'btn btn-sm btn-primary'} onClick={() => showAlert({
+                            title: 'Saved!',
+                            text: 'Successfully saved data',
+                            icon: 'success',
+                            showConfirmButton: false,
+                            timer: 1500,
+                        })}>Show Swal</Button>
+                        <Button className={'btn btn-sm btn-info'} onClick={() => handleAlert({
+                            title: 'Are you sure?',
+                            text: "You won't be able to revert this!",
+                            icon: 'warning',
+                            showCancelButton: true,
+                            confirmButtonText: 'Yes, delete it!',
+                            cancelButtonText: 'No, keep it'
+                        })}>Confirmation Swal</Button>
                     </div>
                 </Card.Body>
             </Card>
