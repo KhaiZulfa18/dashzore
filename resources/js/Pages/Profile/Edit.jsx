d2b876b5327095d611615092b78fc584b9b6be0d
Khai Zulfa <khaizulfa18@gmail.com>
2024-08-01 22:43:07 +0700

    fix profile edit

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -3,34 +3,38 @@ import DeleteUserForm from './Partials/DeleteUserForm';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
 import { Head } from '@inertiajs/react';
+import AppLayout from '@/Layouts/AppLayout';
+import Card from '@/Components/Card';
 
 export default function Edit({ auth, mustVerifyEmail, status }) {
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Profile</h2>}
-        >
-            <Head title="Profile" />
+        <AppLayout>
+            <Head title="My Profile" />
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-                    <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                        <UpdateProfileInformationForm
-                            mustVerifyEmail={mustVerifyEmail}
-                            status={status}
-                            className="max-w-xl"
-                        />
-                    </div>
+            <div className="flex flex-col md:flex-row gap-4 w-full">
 
-                    <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                        <UpdatePasswordForm className="max-w-xl" />
-                    </div>
+            <Card className='w-full md:w-1/2'>
+                <Card.Header>
+                    <h2 className="card-title text-2xl font-bold">Profile</h2>
+                </Card.Header>
+                <Card.Body>
+                    <UpdateProfileInformationForm
+                        mustVerifyEmail={mustVerifyEmail}
+                        status={status}
+                        className="max-w-xl"
+                    />
+                </Card.Body>
+            </Card>
 
-                    <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                        <DeleteUserForm className="max-w-xl" />
-                    </div>
-                </div>
+            <Card className='w-full md:w-1/2'>
+                <Card.Header>
+                    <h2 className="card-title text-2xl font-bold">Password</h2>
+                </Card.Header>
+                <Card.Body>
+                    <UpdatePasswordForm className="max-w-xl" />
+                </Card.Body>
+            </Card>
             </div>
-        </AuthenticatedLayout>
+        </AppLayout>
     );
 }
